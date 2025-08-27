c0c006e2980839655ef2d3e315f9cbc90f02f7c9
Yusif Alaidy <yusifalaidy@gmail.com>
2025-08-27 04:43:41 +0300

    feat(ui,controller): integrate frontend template and add controllers

--- ETickets/ETickets/obj/Debug/net9.0/ETickets.AssemblyInfo.cs
+++ ETickets/ETickets/obj/Debug/net9.0/ETickets.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca06b3279dbe922100511d8ed973eb7cc116bbcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7c36c4d07307b12fbbf95d3b1514fb44cde99a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ETickets/ETickets/wwwroot/js/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.watch').addEventListener('click', function() {
+    alert('Video Link (Trailer) Placeholder');
+});
+document.querySelector('.add-list').addEventListener('click', function() {
+    alert('Added to your list!');
+});
