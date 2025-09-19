396e0e3570b273fc64bfba9bfa98205c12920cba
Yusif Alaidy <yusifalaidy@gmail.com>
2025-09-19 03:22:48 +0300

    feat(admin): create user controller

---
+++ ETickets/ETickets/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETickets.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles = "SuperAdmin")]
+    public class UserController : Controller
+    {
+        #region Fields
+        private readonly UserManager<ApplicationUser> _userManager;
+        #endregion
+        #region constructor
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        #endregion
+        #region Index -> return all users
+        public IActionResult Index()
+        {
+            var users = _userManager.Users;
+
+            return View(users.ToList());
+        }
+        #endregion
+    }
+}
--- ETickets/ETickets/obj/Debug/net9.0/ETickets.AssemblyInfo.cs
+++ ETickets/ETickets/obj/Debug/net9.0/ETickets.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40b4d9095ba72f16877861e716595afa27733920")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6a0784ed87ae71ba1847cd03387252242b5cb29")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
